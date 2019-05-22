e422378576ec209fda9363ace2b524ae6f36027c
Christopher B. Grullon <christopher.gru@gmail.com>
2019-05-21 20:41:26 -0400

    Fixed error with the body params

--- controllers/loanController.js
+++ controllers/loanController.js
@@ -1,7 +1,7 @@
 'use strict'
 const numeral = require('numeral');
 function GenerateTable(req,res){
-    if(!req.body.interes|| !req.body.montoTotal||!req.body.plazo){
+    if(!req.body.interesAnual|| !req.body.montoTotal||!req.body.plazo){
         return res.status(400).send({message:'Parametros invalidos'})
     }
     let montoTotal, interesAnual,plazo,interesMensual,cuotaMensual;
